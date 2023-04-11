093bf007a3bfea44824199cd2d19003337aa0216
Gharabidi Deepak <dgharabidi@gmail.com>
2023-04-11 23:50:23 +0530

    test332

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,50 +1,70 @@
 import React from "react";
-import {Image, View,StyleSheet, ImageBackground, Text} from "react-native";
+import { Image, View, StyleSheet, ImageBackground, Text } from "react-native";
 
-export default function LoginScreen(){
-    return(
-        <View style={styles.mainContainer}>
-            <ImageBackground
-            source={require('../assets/images/bg.png')}
-            resizeMode='cover'
-            style={styles.image}>
-                  {/* innerContainer */}
-                
-            <View style={styles.innerContainer}>
-            <View style={styles.smallContainer}>
-            <Image 
-             style={{width: 90, height: 90, borderRadius:16,}}
-            source={require('../assets/images/app_logo.png')} />
-            <View style={{top:10}}>
+export default function LoginScreen() {
+  return (
+    <View style={styles.mainContainer}>
+      <ImageBackground
+        source={require('../assets/images/bg.png')}
+        resizeMode='cover'
+        style={styles.image}>
+
+        {/* innerContainer */}
+
+        <View style={styles.innerContainer}>
+          <View style={{flexDirection:'row'}}>
+            <Image
+              style={styles.logoImage}
+            //source={require('../assets/images/app_logo.png')}
+            />
+
+           <View style={{justifyContent:'center',}}>
+           <Text>Airline Name</Text>
             <Text>Airline Name</Text>
-            </View>
-            </View>  
-             </View>   
-            </ImageBackground>
+            <Text>Airline Name</Text>
+            <Text>Airline Name</Text>
+            <Text>Airline Name</Text> 
+           </View>
+
+          </View>
         </View>
-    )
+      </ImageBackground>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-        justifyContent: 'center',
-      },
-      innerContainer:{
-        top:140,
-        height:'45%',
-        width:'80%',
-        backgroundColor:'#FFFFFF',
-        alignSelf:'center',
-        borderRadius:16,
-      },
-      smallContainer:{
-        bottom:50,
-        alignSelf:'center',
-        
-      },
-      image: {
-        flex: 1,
-        justifyContent: 'center',
-      },
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  innerContainer: {
+    top: 140,
+    height: '45%',
+    width: '80%',
+    backgroundColor: '#FFFFFF',
+    alignSelf: 'center',
+    borderRadius: 16
+  },
+  smallContainer: {
+    bottom: 50,
+    // alignSelf: 'center',
+
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  logoImage: {
+    elevation: 25,
+    width: 90,
+    height: 90,
+    borderRadius: 16,
+    position: 'absolute',
+    alignSelf: 'center',
+    backgroundColor: '#ff3fff',
+    justifyContent: 'center',
+    //top:-45,
+   
+  }
 })
--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -88,6 +88,7 @@ const SplashScreen = ({navigation}) => {
   }
 
   useEffect(() => {
+    navigation.navigate('LoginScreen')
     //animation()
     fetchToken()
 }, [])
