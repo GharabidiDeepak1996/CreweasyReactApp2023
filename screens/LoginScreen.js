2b0292af74c66da836d9f487b03198cde4e66e7a
Sarvesh Doshi <sdoshi@plexitech.in>
2023-04-12 12:18:24 +0530

    custom ui

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,66 +5,56 @@ export default function LoginScreen() {
   return (
     <View style={styles.mainContainer}>
       <ImageBackground
-        source={require('../assets/images/bg.png')}
-        resizeMode='cover'
-        style={styles.image}>
-
+        source={require("../assets/images/bg.png")}
+        resizeMode="cover"
+        style={styles.backgroundImage}>
+        
         {/* innerContainer */}
-
         <View style={styles.innerContainer}>
-          <View style={{flexDirection:'row'}}>
+          <View style={{ position: "relative", top: -50 }}>
             <Image
               style={styles.logoImage}
-            //source={require('../assets/images/app_logo.png')}
-            />
+              source={require("../assets/images/app_logo.png")}/>
 
-           <View style={{justifyContent:'center',}}>
-           <Text>Airline Name</Text>
             <Text>Airline Name</Text>
             <Text>Airline Name</Text>
             <Text>Airline Name</Text>
-            <Text>Airline Name</Text> 
-           </View>
-
+            <Text>Airline Name</Text>
+            <Text>Airline Name</Text>
           </View>
         </View>
       </ImageBackground>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
   },
   innerContainer: {
     top: 140,
-    height: '45%',
-    width: '80%',
-    backgroundColor: '#FFFFFF',
-    alignSelf: 'center',
-    borderRadius: 16
+    height: "45%",
+    width: "80%",
+    backgroundColor: "#FFFFFF",
+    alignSelf: "center",
+    borderRadius: 16,
   },
-  smallContainer: {
-    bottom: 50,
-    // alignSelf: 'center',
-
-  },
-  image: {
+  backgroundImage: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   logoImage: {
-    elevation: 25,
     width: 90,
     height: 90,
     borderRadius: 16,
-    position: 'absolute',
-    alignSelf: 'center',
-    backgroundColor: '#ff3fff',
-    justifyContent: 'center',
-    //top:-45,
-   
-  }
-})
+    alignSelf: "center",
+    backgroundColor: "#ff3fff",
+    justifyContent: "center",
+  },
+  formattingContainer: {
+    flexDirection: "column",
+  },
+});
